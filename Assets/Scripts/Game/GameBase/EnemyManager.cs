7a1925c2ed8200c4bcb5d51ca5c00f4939fc79a5
LiZhenYu <2383433922@qq.com>
2022-12-22 17:34:46 +0800

    携程生产Enemy

--- Assets/Scripts/Game/GameBase/EnemyManager.cs
+++ Assets/Scripts/Game/GameBase/EnemyManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using ARPG.Config;
 using ARPG.UI.Config;
 using UnityEngine;
@@ -12,6 +14,8 @@ namespace ARPG
     {
         public EnemyConfig config;
         private RegionItem currentRegion;
+        private List<Enemy> CurrenEnemys;
+        private int currentIndex; //当前生产Enemy的波数
 
         protected override void Awake()
         {
@@ -49,6 +53,7 @@ namespace ARPG
         public IEnumerator CreateEnemy(RegionItem data)
         {
             //1. 先把所有的怪物预加载上去
+            currentRegion = data;
             List<Pool.Skill.Pool> CreatBag = new List<Pool.Skill.Pool>();
             
             for (int i = 0; i < data.WaveItems.Count; i++)  //循环每波敌人
@@ -83,13 +88,60 @@ namespace ARPG
         }
         
         
-        
         public void PlayEnemy()
         {
             //如果加载好的新场景是当前数据的目标场景，那么就表示场景加载已完毕，开始准备刷怪
-            UISystem.Instance.DownTime(5,()=>print("刷怪"));
+            currentIndex = 0;
+            UISystem.Instance.DownTime(5,InstanceEnemy);
+        }
+
+        /// <summary>
+        /// 实例化加载Enemy 开始进入状态机状态
+        /// </summary>
+        protected void InstanceEnemy()
+        {
+            //1.首先生成第一波怪物
+            CurrenEnemys = new List<Enemy>();
+            for (int i = 0; i < currentRegion.WaveItems[currentIndex].EnemyList.Count; i++)
+            {
+                EnemyBag data = currentRegion.WaveItems[currentIndex].EnemyList[i];
+                GameObject Prefab = GetPrefab(data.dataID);
+                Vector3 pos = data.CreatePos;
+                int RandomX = (int)Random.Range(-data.Radius, data.Radius);
+                int RandomY = (int)Random.Range(-data.Radius, data.Radius);
+                Enemy enemy =  EnemyPoolManager.Release(Prefab, pos + new Vector3(RandomX, RandomY, 0), Quaternion.identity).GetComponent<Enemy>();
+                enemy.Init(GetData(data.dataID));
+                CurrenEnemys.Add(enemy);
+            }
+            StartCoroutine(WaitCuurrentEnemyDead());
         }
         
+        /// <summary>
+        /// 等待当前列表内怪物全部死亡，开始进行下一波怪物调用
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator WaitCuurrentEnemyDead()
+        {
+            while (CurrenEnemys is { Count: >= 1 })
+            {
+                yield return null;
+            }
+            InstanceEnemy();
+        }
+
+        //TODO: 使用异步编程实现
+        // protected async void WaitEnemyDead()
+        // {
+        //     await Task.Run(delegate
+        //     {
+        //         while (CurrenEnemys is { Count: >= 1 })
+        //         {
+        //             Thread.Sleep(1000);
+        //         }
+        //     });
+        //     
+        //     
+        // }
     }
 }
 
