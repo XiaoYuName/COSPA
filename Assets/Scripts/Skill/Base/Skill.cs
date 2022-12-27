f0ddd9943316ad6a803da0a8cf31176943d3a99b
LiZhenYu <2383433922@qq.com>
2022-12-27 11:00:31 +0800

    删除无用资源

---
+++ Assets/Scripts/Skill/Base/Skill.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using ARPG.Pool.Skill;
+using UnityEngine;
+
+namespace ARPG
+{
+    public abstract class Skill
+    {
+        protected Character Player;
+        protected SkillItem data;
+        protected Enemy Enemy;
+
+        public virtual void Init(Character character, SkillItem item)
+        {
+            Player = character;
+            data = item;
+        }
+
+        public virtual void Init(Enemy enemy, SkillItem item)
+        {
+            Enemy = enemy;
+            data = item;
+        }
+
+        public abstract void Play();
+    }
+
+    /// <summary>
+    /// 战士职业普通攻击
+    /// </summary>
+    public class SkillSoldierAttack : Skill
+    {
+        public override void Play()
+        {
+            Debug.Log("战士职业普通攻击");
+            Player.StartCoroutine(PlayFx());
+        }
+
+        private IEnumerator PlayFx()
+        {
+            yield return new WaitForSeconds(data.ReleaseTime);
+            float rotationY = Player.transform.rotation.eulerAngles.y > 0 ? -90:90; 
+            _FxItem fxItem = SkillPoolManager.Release(data.Pools[0].prefab, Player.body.position,
+                Quaternion.Euler(0,rotationY,-90)).GetComponent<_FxItem>();
+            fxItem.Play(Player,data);
+        }
+    }
+    
+    /// <summary>
+    /// 魔法师职业普通攻击
+    /// </summary>
+    public class SkillConjurerAttack : Skill
+    {
+        public override void Play()
+        {
+            Debug.Log("魔法师职业普通攻击");
+        }
+
+        private IEnumerator PlayFx()
+        {
+            yield return new WaitForSeconds(data.ReleaseTime);
+            
+        }
+    }
+
+    /// <summary>
+    /// 怪物普通攻击
+    /// </summary>
+    public class SkillEnemySoldierAttack : Skill
+    {
+        private Transform AttackPoint;
+        public override void Play()
+        {
+            Enemy.StartCoroutine(PlayFx());
+        }
+        private IEnumerator PlayFx()
+        {
+            AttackPoint = Enemy.transform.Find("AttackPoint");
+            yield return new WaitForSeconds(data.ReleaseTime);
+            _FxItem fxItem = SkillPoolManager.Release(data.Pools[0].prefab, AttackPoint.position,
+                Quaternion.identity).GetComponent<_FxItem>();
+            fxItem.Play(Enemy,data);
+        }
+        
+    }
+}
+
---
+++ Assets/Scripts/Skill/SkillConjurerAttack/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARPG
+{
+    public class Bullet : MonoBehaviour
+    {
+        
+    }
+}
+
+
