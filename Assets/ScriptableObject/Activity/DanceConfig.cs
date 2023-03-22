344d8cd377a0e52fa01d0bea91f0b51fbd674c59
LiZhenYu <2383433922@qq.com>
2023-03-22 18:25:54 +0800

    相关配置变更

--- Assets/ScriptableObject/Activity/DanceConfig.cs
+++ Assets/ScriptableObject/Activity/DanceConfig.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Spine.Unity;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -12,9 +13,14 @@ namespace ARPG.Config
     public class DanceConfig : ScriptableObject
     {
         public List<DanceData> _danceDatas = new List<DanceData>();
+
+        public List<DanceCharacterData> _danceCharacter = new List<DanceCharacterData>();
     }
     
     
+    /// <summary>
+    /// 音乐配置信息
+    /// </summary>
     [System.Serializable]
     public class DanceData
     {
@@ -25,6 +31,41 @@ namespace ARPG.Config
         public string description;
         [Header("Video")]
         public VideoClip VideoID;
+    }
+
+    /// <summary>
+    /// 角色配置信息
+    /// </summary>
+    [System.Serializable]
+    public class DanceCharacterData
+    {
+        [Header("角色名字")]
+        public string CharacterName;
+        
+        [Header("描述")]
+        public string description;
+
+        [Header("Spine动画相关配置")]
+        public SpineAssets spineAssets;
+    }
+
+    [System.Serializable]
+    public class SpineAssets
+    {
+        [Header("主界面选择角色Spine")]
+        public SkeletonDataAsset MainShowSpine;
+        
+        [Header("战斗Spine")]
+        public SkeletonDataAsset mainSpine;
+        
+        [Header("暂未知")]
+        public SkeletonDataAsset ghost;
+
+        [Header("战斗失败Spine")]
+        public SkeletonDataAsset fail;
+
+        [Header("战场胜利Spine")]
+        public SkeletonDataAsset victory;
     }
 }
 
